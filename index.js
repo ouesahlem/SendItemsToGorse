88b44ba21678ab5fd0e24ed5d576f1848bbb6b45
ouesahlem <102174962+ouesahlem@users.noreply.github.com>
2022-06-07 15:41:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function sendItemsToGorse(event: PluginEvent, meta: SendEventsPluginMeta) 
 	const itemID = event.properties?.item_type + '_' + event.properties?.item_id	
 	/*const categories = [event.properties?.item_category]
 	categories.push(event.properties?.item_type)*/
-	const items = new String('{ \"Categories\":' + event.properties?.item_category + ', \"Comment\": \"' + event.properties?.item_price + '\", \"IsHidden\": true, \"ItemId\": \"' + itemID + '\"Labels\": [ \"' + event.properties?.item_name + '\" ], \"Timestamp\": \"' + event.timestamp + '\"}')
+	const items = new String('{ \"Categories\": [ \"' + event.properties?.item_category + '\" ], \"Comment\": \"' + event.properties?.item_price + '\", \"IsHidden\": true, \"ItemId\": \"' + itemID + '\"Labels\": [ \"' + event.properties?.item_name + '\" ], \"Timestamp\": \"' + event.timestamp + '\"}')
 	
 	//fetch : update item
 	await fetch(
